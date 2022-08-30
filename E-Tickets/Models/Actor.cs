d4a6b422e95cd001b8c87b8d2482344939df597a
Amit Rawat <beingamit4197@gmail.com>
2022-08-30 12:39:19 +0530

    Added Actor and Producer Model

---
+++ E-Tickets/Models/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tickets.Models
+{
+    public class Actor
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string ProfilePictureUrl { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Bio { get; set; }
+    }
+}
