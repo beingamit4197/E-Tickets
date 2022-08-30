2f1b00dda1f97add41b33c0904abe9a18e84479c
Amit Rawat <beingamit4197@gmail.com>
2022-08-30 12:57:40 +0530

    Added Cinema/movie/movieCategory models

---
+++ E-Tickets/Data/MovieCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tickets.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1, Comedy, Drama, Documentory, Horror
+    }
+}
---
+++ E-Tickets/Models/Cinema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tickets.Models
+{
+    public class Cinema
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Logo { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
