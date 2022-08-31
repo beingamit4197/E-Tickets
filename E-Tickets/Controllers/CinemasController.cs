81edcd6eae1f25a41f5e0915d207fee8f3dcac0e
Amit Rawat <beingamit4197@gmail.com>
2022-08-31 10:35:25 +0530

    Added CinemasController

---
+++ E-Tickets/Controllers/CinemasController.cs
@@ -0,0 +1,27 @@
+﻿using E_Tickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allProducers = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
