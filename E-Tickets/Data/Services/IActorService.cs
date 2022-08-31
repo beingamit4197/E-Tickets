96c4e559c28c3ad844f244d582d794658682cf65
Amit Rawat <beingamit4197@gmail.com>
2022-08-31 15:17:57 +0530

    Added IActorServices

---
+++ E-Tickets/Data/Services/IActorService.cs
@@ -0,0 +1,23 @@
+﻿using E_Tickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Tickets.Data.Services
+{
+    interface IActorService
+    {
+
+        IEnumerable<Actor> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        Actor Update(int id, Actor newActor);
+
+        void Delete(int id);
+
+    }
+}
