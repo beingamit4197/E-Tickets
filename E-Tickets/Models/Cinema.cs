6ca308028592e64b254367a03d757a4b724f9225
Amit Rawat <beingamit4197@gmail.com>
2022-08-31 14:08:32 +0530

    Added a view for cinemas

--- E-Tickets/Models/Cinema.cs
+++ E-Tickets/Models/Cinema.cs
@@ -12,10 +12,14 @@ namespace E_Tickets.Models
 
         public int Id { get; set; }
 
+        [Display(Name = "Logo")]
         public string Logo { get; set; }
 
+        
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
+        [Display(Name = "Desccription")]
         public string Description { get; set; }
 
         public List<Movie> Movies { get; set; }
